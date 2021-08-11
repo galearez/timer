ccbe50d7e16099c586f236aaae4080ff1cc8103b
Joseph <j297g594@gmail.com>
2021-08-10 18:32:54 -0600

    Manually created arrays for time select boxes

--- src/timer.tsx
+++ src/timer.tsx
@@ -23,6 +23,18 @@ interface ITimerStates {
   closeHomeScreen: boolean;
 }
 
+//before this change this two arrays where auto-generated on component mounting, but there was a big problem
+//it needs to 'update' before generating the options for the select box so when using it on desktop there
+//are not options at the start, thats why even if it's annoying I created this two arrays so there
+//are options on first rendering
+const FIVE_BY_FIVE = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60];
+const ONE_BY_ONE = [
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+  22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+  41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59,
+  60,
+];
+
 //this component will handle the user input and will pass main data, rounds (id, label and time)
 //to the other components
 export default class Timer extends React.Component<{}, ITimerStates> {
@@ -95,10 +107,6 @@ export default class Timer extends React.Component<{}, ITimerStates> {
   //variable to keep track of the number of rounds, just for rounds default names
   roundDefaultNumber: number = 1;
 
-  //this will help to auto-generate the options in the select boxes
-  oneToSixtyArray: number[] = [];
-  fiveByFiveArray: number[] = [];
-
   //when screen resize it will be called to change the screenWidth state which control some responsive rendering
   handleScreenResize() {
     this.setState({ screenWidth: window.innerWidth }, () => {
@@ -121,15 +129,6 @@ export default class Timer extends React.Component<{}, ITimerStates> {
   }
 
   componentDidMount() {
-    //once mounted generate arrays with the options for the select boxes, I'm lazy
-    for (let i = 0; i <= 60; i++) {
-      this.oneToSixtyArray.push(i);
-    }
-
-    for (let i = 0; i <= 12; i++) {
-      this.fiveByFiveArray.push(i * 5);
-    }
-
     //once it gets mounted waits for any resize
     window.addEventListener('resize', this.handleScreenResize);
   }
@@ -322,14 +321,14 @@ export default class Timer extends React.Component<{}, ITimerStates> {
 
   render() {
     //creates options for the minutes select box
-    const sixtyOptions = this.oneToSixtyArray.map((elem: number) => (
+    const sixtyOptions = ONE_BY_ONE.map((elem: number) => (
       <option value={elem} key={elem}>
         {elem}
       </option>
     ));
 
     //create options for the seconds select box
-    const twelveOptions = this.fiveByFiveArray.map((elem: number) => (
+    const twelveOptions = FIVE_BY_FIVE.map((elem: number) => (
       <option value={elem} key={elem}>
         {elem}
       </option>
