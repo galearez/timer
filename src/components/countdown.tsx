48037faf6f18c79104542ad7472b098a940c275a
galearez <51718252+galearez@users.noreply.github.com>
2022-02-11 21:59:54 -0600

    Countdown Functinoal Component draft
    This is the first draft of what will the countdown
    component be once I refactor everything using hooks

--- src/components/countdown.tsx
+++ src/components/countdown.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Icons from './icons';
 
 interface ICountdownProps {
@@ -16,6 +16,8 @@ interface ICountdownProps {
   buttonsToMove: number;
   //if the routine is to short, maybe one round, we don't need buttons to move over the rounds
   disbaleMoveButtons?: boolean;
+
+  routine?: IRound[];
 }
 
 interface ICountdownState {
@@ -29,10 +31,14 @@ interface ICountdownState {
   isPaused: boolean;
 }
 
-export default class Countdown extends React.Component<
-  ICountdownProps,
-  ICountdownState
-> {
+//This is the type of the values stored in routine
+interface IRound {
+  id: number;
+  label: string;
+  time: number;
+}
+
+class CountdownClass extends React.Component<ICountdownProps, ICountdownState> {
   constructor(props: any) {
     super(props);
 
@@ -188,3 +194,166 @@ export default class Countdown extends React.Component<
     );
   }
 }
+
+// Yasta solo limpio y ya quedó el primer functional component, Noice
+
+function Countdown(props: ICountdownProps) {
+  const routine = props.routine;
+  const time2 = routine ? routine[0].time : 0;
+
+  let [countdownTime, setCountdownTime] = useState(time2);
+  let [minutes, setMinutes] = useState(Math.floor(props.time / 60));
+  let [seconds, setSeconds] = useState(props.time % 60);
+  let [isPaused, setIsPaused] = useState(false);
+
+  let [round, setRound] = useState(0);
+
+  //this class variable will hold the setInterval, their only purpose is to be able to clear the interval
+  let interval = useRef<number>();
+
+  //this variable will hold the last time state when the timer is paused
+  let time: number;
+
+  //once the component mounts it will start a countdown, the original value is the one passed as a prop to
+  //the component and it will run until the 'countdownTime' is 0
+  useEffect(() => {
+    interval.current = window.setInterval(() => {
+      if (countdownTime <= 0) {
+        setRound((prevStatae) => prevStatae + 1);
+        clearInterval(interval.current);
+      } else {
+        setCountdownTime(countdownTime - 1);
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(interval.current);
+    };
+  }, [countdownTime]);
+
+  useEffect(() => {
+    setMinutes(Math.floor(countdownTime / 60));
+    setSeconds(countdownTime % 60);
+  }, [countdownTime]);
+
+  useEffect(() => {
+    if (routine && round < routine.length) {
+      setCountdownTime(routine[round].time);
+    }
+  }, [round, routine]);
+
+  let propNextRound = props.nextRound;
+  let propUnmountCountdown = props.unmountCountdown;
+  useEffect(() => {
+    if (!routine) {
+      return;
+    }
+
+    if (routine?.length < round) {
+      propNextRound(1);
+    }
+
+    if (round >= routine?.length) {
+      propUnmountCountdown();
+    }
+  }, [routine, round, propNextRound, propUnmountCountdown]);
+
+  //this function will remove the setInterval to stop the countdown
+  function stopCountdown() {
+    clearInterval(interval.current);
+    setCountdownTime(0);
+    time = countdownTime;
+    setIsPaused(true);
+  }
+
+  //this function will restart the countdown
+  function resumeCountdown() {
+    setIsPaused(false);
+    setCountdownTime(time);
+  }
+
+  //this function will restart the current round
+  function restartRound() {
+    if (routine) {
+      setCountdownTime(routine[round].time);
+    }
+
+    return;
+  }
+
+  //this function will unmount the current round and will mount the next round if there is one
+  function nextRound() {
+    setRound(round + 1);
+
+    if (routine) {
+      setCountdownTime(routine[round].time);
+    }
+  }
+  //this function will unmount the current round and will mount the previous round if there is one
+  function previousRound() {
+    setRound(round + 1);
+
+    if (routine) {
+      setCountdownTime(routine[round].time);
+    }
+  }
+
+  //the buttons will now be controlled by the component, since I now moved the logic to navigate through
+  //the routine to this component instead of the timer component
+
+  return (
+    <div className='font-bold h-80 flex flex-col justify-center items-center'>
+      <div className='text-2xl md:text-4xl'>{props.label}</div>
+      <div className='text-6xl md:text-8xl mb-10'>
+        <span>
+          {minutes < 10 ? <span>0{minutes}</span> : <span>{minutes}</span>}:
+          {seconds < 10 ? <span>0{seconds}</span> : <span>{seconds}</span>}
+        </span>
+      </div>
+      <div className='w-full max-w-md flex justify-around items-center'>
+        {!props.disbaleMoveButtons && (
+          <button
+            className='rounded-full w-12 h-12 bg-gray-700 disabled:bg-gray-900'
+            disabled={props.buttonsToMove === -1}
+            onClick={previousRound}
+          >
+            <Icons value={'previous'} disbale={props.buttonsToMove === -1} />
+          </button>
+        )}
+
+        <button
+          className='rounded-full w-14 h-14 bg-gray-700'
+          onClick={restartRound}
+        >
+          <Icons value={'replay'} />
+        </button>
+        {isPaused ? (
+          <button
+            className='rounded-full w-16 h-16 bg-mint'
+            onClick={resumeCountdown}
+          >
+            <Icons value={'play'} />
+          </button>
+        ) : (
+          <button
+            className='rounded-full w-16 h-16 bg-gray-700'
+            onClick={stopCountdown}
+          >
+            <Icons value={'pause'} />
+          </button>
+        )}
+        {!props.disbaleMoveButtons && (
+          <button
+            className='rounded-full w-12 h-12 bg-gray-700 disabled:bg-gray-900'
+            disabled={props.buttonsToMove === 1}
+            onClick={nextRound}
+          >
+            <Icons value={'next'} disbale={props.buttonsToMove === 1} />
+          </button>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Countdown;
