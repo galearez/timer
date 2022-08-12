30c80e5345aab922d3af53ad58ee58a2935a7b9c
Josep Galearez <51718252+galearez@users.noreply.github.com>
2022-08-11 20:11:53 -0600

    move NextActivity component to its own file

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -9,6 +9,7 @@ import ActivitiesList from './activities-list';
 import clsx from 'clsx';
 
 import Icons from '../utils/icons';
+import NextActivity from './next-activity';
 
 // this context will pass the control of the rest states to the global rest component
 export const RestContext = React.createContext({
@@ -149,45 +150,6 @@ export default function App() {
       </div>
       <div className='mt-2'>
         <ActivitiesList activities={routine} />
-      </div>
-    </div>
-  );
-}
-
-// this variable is meant to show the next round on the routine
-interface Activities {
-  id: string;
-  label: string;
-  time: number;
-}
-
-interface NextActivityProps {
-  currentActivity: number;
-  routine: Activities[];
-}
-
-function NextActivity(props: NextActivityProps) {
-  const { currentActivity, routine } = props;
-
-  if (currentActivity >= routine.length - 1) {
-    return <div className='h-20 w-1 mb-4'></div>;
-  }
-  const elem = routine[currentActivity + 1];
-  const minutes = Math.floor(elem.time / 60);
-  const seconds = elem.time % 60;
-
-  return (
-    <div className='bg-gray-700 rounded-md mb-4'>
-      <h2 className='pt-2 px-2'>Next</h2>
-
-      <div className='w-full p-2 flex justify-between items-center flex-nowrap text-lg'>
-        <span>{elem.label}</span>
-        <div className='flex items-center'>
-          <span>
-            {minutes < 10 ? <span>0{minutes}</span> : <span>{minutes}</span>}:
-            {seconds < 10 ? <span>0{seconds}</span> : <span>{seconds}</span>}
-          </span>
-        </div>
       </div>
     </div>
   );
